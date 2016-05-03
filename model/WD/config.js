8a1620c11667a5a99be0ea06f194cda12dd0abc2
Renato Iannella <renato.iannella@monegraph.com>
2016-05-03 10:44:42 +1000

    added editors

--- model/WD/config.js
+++ model/WD/config.js
@@ -3,19 +3,25 @@
           shortName:  "odrl-model",
           editors: [
                 {   name:       "Renato Iannella",
-                    url:        "http://iannel.la/",
+                    url:        "https://au.linkedin.com/in/riannella",
                     company:    "Monegraph",
                     companyURL: "http://www.monegraph.com/",
                     mailto:     "r@iannel.la"
+                },
+               {   name:        "Serena Villata",
+                    url:        "https://www.linkedin.com/in/serenavillata",
+                    company:    "INRIA",
+                    companyURL: "http://www.inria.fr/en/",
+                    mailto:     "serena.villata@inria.fr"
                 }
           ],
           previousMaturity:     "",
           previousPublishDate:  "",
           previousURI:          "",
-          publishDate:          "2016-04-19",
+          publishDate:          "2016-05-01",
           edDraftURI:           "http://w3c.github.io/poe/model/",
           wg:                   "Permissions & Obligations Expression Working Group",
-          wgURI:                "http://www.w3.org/poe/",
+          wgURI:                "http://www.w3.org/2016/poe/",
           wgPublicList:         "public-poe",
           wgPatentURI:          "http://www.w3.org/2004/01/pp-impl/73180/status",
           localBiblio: {
--- vocab/WD/config.js
+++ vocab/WD/config.js
@@ -2,36 +2,34 @@ var respecConfig = {
     specStatus: "ED",
     shortName: "odrl-vocab",
     //publishDate:  "2015-05-18",
-    //previousPublishDate: "2014-03-27",
-    //previousMaturity: "FPWD",
-    //previousURI: "http://www.w3.org/TR/2014/WD-tabular-data-model-20140327/",
+    //previousPublishDate: "",
+    //previousMaturity: "",
+    //previousURI: "",
     edDraftURI: "https://w3c.github.io/poe/vocab/",
     // lcEnd: "3000-01-01",
     // crEnd: "3000-01-01",
     editors: [
-     {   name:       "Bart",
-                    url:        "",
-                    company:    "",
-                    companyURL: "",
-                    mailto:     ""
+     {    name:       "Renato Iannella",
+                    url:        "https://au.linkedin.com/in/riannella",
+                    company:    "Monegraph",
+                    companyURL: "http://www.monegraph.com/",
+                    mailto:     "r@iannel.la"
                 },
-      {   name:       "Homer",
-                    url:        "",
-                    company:    "",
-                    companyURL: "",
-                    mailto:     ""
+      {    name:       "Michael Steidl",
+                    url:        "https://uk.linkedin.com/in/michaelwsteidl",
+                    company:    "Invited Expert",
+                    mailto:     "mdirector@iptc.org"
                 },
-         {   name:       "Lisa",
-                    url:        "",
-                    company:    "",
-                    companyURL: "",
-                    mailto:     ""
+         {   name:       "Mo McRoberts",
+                    url:        "https://uk.linkedin.com/in/nevali",
+                    company:    "BBC",
+                    companyURL: "http://www.bbc.co.uk",
+                    mailto:     "mo.mcroberts@bbc.co.uk"
                 },
-      {   name:       "Marge",
-                    url:        "",
-                    company:    "",
-                    companyURL: "",
-                    mailto:     ""
+      {  name:       "Stuart Myles",
+                    url:        "https://www.linkedin.com/in/stuart-myles-60a985",
+                    company:    "Invited Expert",
+                    mailto:     "SMyles@ap.org"
                 }],
     wg: "Permissions &amp; Obligations Expression Working Group",
     wgURI: "https://www.w3.org/2016/poe/",
@@ -40,6 +38,5 @@ var respecConfig = {
     inlineCSS: true,
     noIDLIn: true,
     noLegacyStyle: false,
-    noRecTrack: true,
     issueBase: "https://www.w3.org/2016/poe/track/issues/"
 };
