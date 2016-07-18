435da6de74ec04e723dba45ee6ed3906488c0328
Phil Archer <phila@w3.org>
2016-07-18 16:54:16 +0100

    Creating Model FPWD Snapshot, updating index

---
+++ snapshots/WD-odrl-model-2016-07-21/config.js
@@ -0,0 +1,42 @@
+      var respecConfig = {
+          specStatus: "FPWD",
+          shortName:  "odrl-model",
+          editors: [
+                {   name:       "Renato Iannella",
+                    url:        "https://au.linkedin.com/in/riannella",
+                    company:    "Monegraph",
+                    companyURL: "http://www.monegraph.com/",
+                    mailto:     "r@iannel.la"
+                },
+               {   name:        "Serena Villata",
+                    url:        "https://www.linkedin.com/in/serenavillata",
+                    company:    "INRIA",
+                    companyURL: "http://www.inria.fr/en/",
+                    mailto:     "serena.villata@inria.fr"
+                }
+          ],
+          lint:                 true,
+//          previousMaturity:     "",
+//          previousPublishDate:  "",
+//          previousURI:          "",
+          publishDate:          "2016-07-21",
+          edDraftURI:           "https://w3c.github.io/poe/model/",
+          wg:                   "Permissions & Obligations Expression Working Group",
+          wgURI:                "https://www.w3.org/2016/poe/",
+          wgPublicList:         "public-poe",
+          wgPatentURI:          "https://www.w3.org/2004/01/pp-impl/73180/status",
+          issueBase:            "https://github.com/w3c/poe/issues/",
+          otherLinks: [
+                {
+                        key: "Repository",
+                        data: [{
+                                value: "Github Repository",
+                                href: "https://github.com/w3c/poe"
+                        },
+                              {
+                                value: "Issue Tracker",
+                                href: "https://github.com/w3c/poe/milestone/1"
+                        }]
+                }
+          ]
+      };
