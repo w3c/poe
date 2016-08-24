e50376832d5e93d1170e6d57642d989bddf8b1d9
Renato Iannella <r@iannel.la>
2016-08-24 21:46:15 +1000

    Changed the Scenario section to reference the source text from /examples in vocab. Added links from the scenarios to vocab encoding examples

--- model/config.js
+++ model/config.js
@@ -1,7 +1,7 @@
       var respecConfig = {
           specStatus: "ED",
           shortName:  "odrl-model",
-          publishDate:          "2016-08-15",
+          publishDate:          "2016-08-24",
           editors: [
                 {   name:       "Renato Iannella",
                     url:        "https://au.linkedin.com/in/riannella",
