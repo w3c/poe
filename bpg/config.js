7895b971414d4ab2a938c79c58dd44ffae04ab6d
Renato Iannella <r@iannel.la>
2016-10-19 14:12:37 +1000

    Added the template for the ODRL Formal Semantics and the ODRL Best Practices Guide NOTES

---
+++ bpg/config.js
@@ -0,0 +1,26 @@
+var respecConfig = {
+    specStatus: "ED",
+    shortName: "odrl-bpg",
+     publishDate:          "2016-10-18",
+    edDraftURI: "https://w3c.github.io/poe/bpg/",
+   editors: [	{   name:       "Benedict Whittam Smith",
+         	company:    "Thomson Reuters"},
+         	{   name:       "Paul Jessop",
+         	company:    "Recording Industry Association of America"},
+         	 {  name:       "Víctor Rodríguez-Doncel",
+                    url:        "https://www.linkedin.com/in/victor-rodriguez-doncel-554b5213/en",
+                    company:    "Universidad Politécnica de Madrid",
+                   companyURL: "http://www.upm.es",
+                    mailto:     "vrodriguez@fi.upm.es"
+           }
+         	],
+    wg: "Permissions &amp; Obligations Expression Working Group",
+    wgURI: "https://www.w3.org/2016/poe/",
+    wgPublicList: "public-poe-comments",
+    wgPatentURI: "http://www.w3.org/2004/01/pp-impl/87755/status",
+    inlineCSS: true,
+    noIDLIn: true,
+    noLegacyStyle: false,
+    noRecTrack: true,
+    issueBase: "https://www.w3.org/2016/poe/track/issues/"
+};
---
+++ semantics/config.js
@@ -0,0 +1,27 @@
+var respecConfig = {
+    specStatus: "ED",
+    shortName: "odrl-semantics",
+     publishDate:          "2016-10-18",
+    edDraftURI: "https://w3c.github.io/poe/semantics/",
+   editors: [	{   name:       "Simon Steyskal",
+         	url:        "http://steyskal.info/",
+         	company:    "WU Vienna/Siemens AG"},
+         	{   name:       "Sabrina Kirrane",
+         	company:    "WU Vienna"},
+         	 {  name:       "Víctor Rodríguez-Doncel",
+                    url:        "https://www.linkedin.com/in/victor-rodriguez-doncel-554b5213/en",
+                    company:    "Universidad Politécnica de Madrid",
+                   companyURL: "http://www.upm.es",
+                    mailto:     "vrodriguez@fi.upm.es"
+           }
+         	],
+    wg: "Permissions &amp; Obligations Expression Working Group",
+    wgURI: "https://www.w3.org/2016/poe/",
+    wgPublicList: "public-poe-comments",
+    wgPatentURI: "http://www.w3.org/2004/01/pp-impl/87755/status",
+    inlineCSS: true,
+    noIDLIn: true,
+    noLegacyStyle: false,
+    noRecTrack: true,
+    issueBase: "https://www.w3.org/2016/poe/track/issues/"
+};
