6f2a6bb3a17adef9964f9d59f01999e693f601bd
Renato Iannella <r@iannel.la>
2016-12-12 15:33:10 +1000

    Small update to what is being identified in policy inheritance

--- model/config.js
+++ model/config.js
@@ -1,7 +1,7 @@
       var respecConfig = {
           specStatus: "ED",
           shortName:  "odrl-model",
-          publishDate:          "2016-12-06",
+          publishDate:          "2016-12-12",
           editors: [
                 {   name:       "Renato Iannella",
                     url:        "https://au.linkedin.com/in/riannella",
