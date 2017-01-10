50e52ad840c2da9c410350055c647c6a8b253ac9
Renato Iannella <r@iannel.la>
2017-01-10 12:06:54 +1000

    Added inverse target asset to the rdf encoding section

--- vocab/config.js
+++ vocab/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
     specStatus: "ED",
     shortName: "vocab-odrl",
-    publishDate:  "2017-01-03",
+    publishDate:  "2017-01-10",
     edDraftURI: "https://w3c.github.io/poe/vocab/",
     editors: [
      {    name:       "Renato Iannella",
