a7c653b80fc274a828b62b8f713d4a7ee4801076
Renato Iannella <r@iannel.la>
2017-01-18 11:49:42 +1000

    Removed scenario section (add to BP Note)

--- vocab/config.js
+++ vocab/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
     specStatus: "ED",
     shortName: "vocab-odrl",
-    publishDate:  "2017-01-17",
+    publishDate:  "2017-01-18",
     edDraftURI: "https://w3c.github.io/poe/vocab/",
     editors: [
      {    name:       "Renato Iannella",
