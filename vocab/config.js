5fb3674c21e8a943dac8c7df3aede03a2f000f99
Renato Iannella <r@iannel.la>
2017-01-24 13:50:17 +1000

    updated the operator definitions

--- vocab/config.js
+++ vocab/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
     specStatus: "ED",
     shortName: "vocab-odrl",
-    publishDate:  "2017-01-22",
+    publishDate:  "2017-01-24",
     edDraftURI: "https://w3c.github.io/poe/vocab/",
     editors: [
      {    name:       "Renato Iannella",
