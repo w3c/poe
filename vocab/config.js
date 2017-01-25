ac75709a0383450cfd0620a922b40abed12c6e08
Renato Iannella <r@iannel.la>
2017-01-25 12:17:20 +1000

    Updated spatial constraints

--- vocab/config.js
+++ vocab/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
     specStatus: "ED",
     shortName: "vocab-odrl",
-    publishDate:  "2017-01-24",
+    publishDate:  "2017-01-25",
     edDraftURI: "https://w3c.github.io/poe/vocab/",
     editors: [
      {    name:       "Renato Iannella",
