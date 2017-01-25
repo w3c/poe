71f60ee3b2e118a59a9e035cd30d63447bf2e12b
Renato Iannella <r@iannel.la>
2017-01-25 14:07:08 +1000

    Updated Constraint narrative to support Constraint Objects as left/right operands

--- model/config.js
+++ model/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
         specStatus: "ED",
         shortName:  "odrl-model",
-        publishDate:          "2017-01-24",
+        publishDate:          "2017-01-25",
         editors: [
             {   name:       "Renato Iannella",
                     url:        "https://au.linkedin.com/in/riannella",
