f051c0e61918d5fe22982487a303c201ad3c0660
Renato Iannella <r@iannel.la>
2017-01-27 12:47:00 +1000

    Updated Constraints section

--- vocab/config.js
+++ vocab/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
     specStatus: "ED",
     shortName: "vocab-odrl",
-    publishDate:  "2017-01-25",
+    publishDate:  "2017-01-27",
     edDraftURI: "https://w3c.github.io/poe/vocab/",
     editors: [
      {    name:       "Renato Iannella",
