a2e37b85ebb8315661e9c767b561170b539fa011
Renato Iannella <r@iannel.la>
2017-01-31 13:56:14 +1000

    Updated isNecessaryFor definition and narrative. Added more processing rules to Constraint Relations.

--- model/config.js
+++ model/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
         specStatus: "ED",
         shortName:  "odrl-model",
-        publishDate:          "2017-01-30",
+        publishDate:          "2017-01-31",
         editors: [
             {   name:       "Renato Iannella",
                     url:        "https://au.linkedin.com/in/riannella",
