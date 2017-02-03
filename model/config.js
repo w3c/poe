65d4ffd4624a8673a1a17ab1bcb3e9807446b53e
Renato Iannella <r@iannel.la>
2017-02-03 11:58:33 +1000

    fixed name->left operand

--- model/config.js
+++ model/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
         specStatus: "ED",
         shortName:  "odrl-model",
-        publishDate:          "2017-02-02",
+        publishDate:          "2017-02-03",
         editors: [
             {   name:       "Renato Iannella",
                     url:        "https://au.linkedin.com/in/riannella",
