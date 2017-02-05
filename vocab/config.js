be033f6be8315adf43d338465b28815e0aa0066f
Renato Iannella <r@iannel.la>
2017-02-05 22:03:47 +1000

    Updated Constraint Relations section in the IM (see #98). Aligned some of the definitions between the IM and the Vocab (see #102)

--- vocab/config.js
+++ vocab/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
     specStatus: "ED",
     shortName: "vocab-odrl",
-    publishDate:  "2017-02-01",
+    publishDate:  "2017-02-05",
     edDraftURI: "https://w3c.github.io/poe/vocab/",
     editors: [
      {    name:       "Renato Iannella",
