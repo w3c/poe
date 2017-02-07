4a7ac54fe2fe2e71387ba3736e37016a1cb827f6
Renato Iannella <r@iannel.la>
2017-02-07 13:57:00 +1000

    Updated Permissions section

--- model/config.js
+++ model/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
         specStatus: "ED",
         shortName:  "odrl-model",
-        publishDate:          "2017-02-05",
+        publishDate:          "2017-02-07",
         editors: [
             {   name:       "Renato Iannella",
                     url:        "https://au.linkedin.com/in/riannella",
