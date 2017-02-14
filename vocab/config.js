dea92514de09e8ca4dff2fd32ad36bd5b9ff3bfe
Renato Iannella <r@iannel.la>
2017-02-14 12:17:34 +1000

    Removed “to an audience” from terms #101. Added note.

--- vocab/config.js
+++ vocab/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
     specStatus: "ED",
     shortName: "vocab-odrl",
-    publishDate:  "2017-02-13",
+    publishDate:  "2017-02-14",
     edDraftURI: "https://w3c.github.io/poe/vocab/",
     editors: [
      {    name:       "Renato Iannella",
