f63ce3000f14263b6818715309f7a3695f1f743b
Renato Iannella <r@iannel.la>
2017-02-16 13:19:13 +1000

    Fixed the references in the Vocab/Model. Removed unused nano tab script. Used real URIs for currency/country in examples in Model.

--- model/config.js
+++ model/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
         specStatus: "ED",
         shortName:  "odrl-model",
-        publishDate:          "2017-02-14",
+        publishDate:          "2017-02-16",
         editors: [
             {   name:       "Renato Iannella",
                     url:        "https://au.linkedin.com/in/riannella",
