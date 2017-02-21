30ea18d3f27686ff942fa15e7cafa5f1b6fbde01
Simon Steyskal <simon.steyskal@gmail.com>
2017-02-21 07:20:53 +0100

    more work on ucr

--- ucr/config.js
+++ ucr/config.js
@@ -2,7 +2,6 @@ var respecConfig = {
 	preProcess: [dfn_index],
     specStatus: "WD",
     shortName: "poe-ucr",
-	publishDate:  "2017-02-21",
     previousMaturity:     "NOTE",
     previousPublishDate:  "2016-07-21",
     previousURI:          "https://www.w3.org/TR/2016/NOTE-poe-ucr-20160721/",
