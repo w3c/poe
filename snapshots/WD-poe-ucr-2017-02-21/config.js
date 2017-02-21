b2e4fbb1bc1363b3ffa390daff3c8e370ce3fb6a
Simon Steyskal <simon.steyskal@gmail.com>
2017-02-21 06:59:39 +0100

    added ucr snapshot

---
+++ snapshots/WD-poe-ucr-2017-02-21/config.js
@@ -0,0 +1,35 @@
+var respecConfig = {
+	preProcess: [dfn_index],
+    specStatus: "ED",
+    shortName: "poe-ucr",
+    //publishDate:  "2016-07-21",
+    //previousPublishDate: "2014-03-27",
+    //previousMaturity: "FPWD",
+    //previousURI: "http://www.w3.org/TR/2014/WD-tabular-data-model-20140327/",
+    edDraftURI: "https://w3c.github.io/poe/ucr/",
+    // lcEnd: "3000-01-01",
+    // crEnd: "3000-01-01",
+   editors: [{   name:       "Michael Steidl",
+         	company:    "The Copyright Hub"},
+         	{   name:       "Simon Steyskal",
+         	url:        "http://steyskal.info/",
+         	company:    "WU Vienna/Siemens AG"},
+         	{   name:       "Benedict Whittam Smith",
+         	company:    "Thomson Reuters"}
+         	],
+    wg: "Permissions &amp; Obligations Expression Working Group",
+    wgURI: "https://www.w3.org/2016/poe/",
+    wgPublicList: "public-poe-comments",
+    wgPatentURI: "https://www.w3.org/2004/01/pp-impl/87755/status",
+    inlineCSS: true,
+    noIDLIn: true,
+    noLegacyStyle: false,
+    noRecTrack: true,
+    localBiblio: {
+      "SCHEMA-ORG":{
+        "href":"http://schema.org/",
+        "title":"Schema.org"
+       },
+    },
+    issueBase: "https://www.w3.org/2016/poe/track/issues/"
+};
