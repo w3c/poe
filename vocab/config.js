b6f3f1ae61e9bf81fc122f66bd7c105b7040ae6d
Renato Iannella <r@iannel.la>
2017-03-20 13:02:26 +1000

    Fixed some accessibility issue#114 in the vocab

--- vocab/config.js
+++ vocab/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
     specStatus: "ED",
     shortName: "odrl-vocab",
-    publishDate:  "2017-02-23",
+    publishDate:  "2017-03-20",
     edDraftURI: "https://w3c.github.io/poe/vocab/",
     editors: [
      {    name:       "Renato Iannella",
