b0b7223ba42b8293cab82cdc355f6298cd3e9c68
Renato Iannella <r@iannel.la>
2017-03-20 12:15:27 +1000

    Fixed <b> and <i> for <strong> and <em> Issue#114

--- model/config.js
+++ model/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
         specStatus: "ED",
         shortName:  "odrl-model",
-        publishDate:          "2017-03-03",
+        publishDate:          "2017-03-20",
         editors: [
             {   name:       "Renato Iannella",
                     url:        "https://au.linkedin.com/in/riannella",
