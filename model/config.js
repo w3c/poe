b4aa57d0e360a51a0fc0ecbeebb9cd698671f7f5
Renato Iannella <r@iannel.la>
2017-04-21 12:07:18 +1000

    added <code> to hrefs

--- model/config.js
+++ model/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
         specStatus: "ED",
         shortName:  "odrl-model",
-        publishDate:          "2017-04-19",
+        publishDate:          "2017-04-21",
         editors: [
             {   name:       "Renato Iannella",
                     url:        "https://au.linkedin.com/in/riannella",
