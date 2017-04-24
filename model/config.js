f1512a8a23ac1069fc13e4f52c613d0ebc548a77
Renato Iannella <r@iannel.la>
2017-04-24 11:35:49 +1000

    Added ODRL Processor to terminology

--- model/config.js
+++ model/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
         specStatus: "ED",
         shortName:  "odrl-model",
-        publishDate:          "2017-04-21",
+        publishDate:          "2017-04-24",
         editors: [
             {   name:       "Renato Iannella",
                     url:        "https://au.linkedin.com/in/riannella",
