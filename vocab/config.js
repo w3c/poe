5115b01250e99500eb24e08fd03402ea794c5a72
Renato Iannella <r@iannel.la>
2017-04-26 11:51:30 +1000

    Added Privacy Considerations section to both documents.  Added use of UTF-8 for encoding. Update more URIs to IRIs.

--- vocab/config.js
+++ vocab/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
     specStatus: "ED",
     shortName: "odrl-vocab",
-    publishDate:  "2017-04-11",
+    publishDate:  "2017-04-26",
     edDraftURI: "https://w3c.github.io/poe/vocab/",
     editors: [
      {    name:       "Renato Iannella",
