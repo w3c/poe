18a69324aa824e24737dc0dff6db3d3019043ab1
Renato Iannella <r@iannel.la>
2017-04-28 15:36:42 +1000

    Update the vocab with Atomic/Compound Constraint operators. Updated figure.

--- vocab/config.js
+++ vocab/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
     specStatus: "ED",
     shortName: "odrl-vocab",
-    publishDate:  "2017-04-26",
+    publishDate:  "2017-04-28",
     edDraftURI: "https://w3c.github.io/poe/vocab/",
     editors: [
      {    name:       "Renato Iannella",
