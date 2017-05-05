3a11111d80d6db29d13a10e88d99aff6346ca1cf
Renato Iannella <r@iannel.la>
2017-05-05 13:18:20 +1000

    Editorial update to IM as per #162

--- model/config.js
+++ model/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
         specStatus: "ED",
         shortName:  "odrl-model",
-        publishDate:          "2017-05-04",
+        publishDate:          "2017-05-05",
         editors: [
             {   name:       "Renato Iannella",
                     url:        "https://au.linkedin.com/in/riannella",
