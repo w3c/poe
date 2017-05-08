8d5ac83953e522900173c40f8d3671b4da285cf6
Phil Archer <phila@w3.org>
2017-05-08 15:20:48 +0100

    Forgot the config file

---
+++ testsuite/config.js
@@ -0,0 +1,26 @@
+var respecConfig = {
+//	preProcess: [dfn_index],
+    specStatus: "ED",
+    shortName: "odrl-tests",
+//    previousMaturity:     "NOTE",
+//    previousPublishDate:  "2016-07-23",
+//    previousURI:          "https://www.w3.org/TR/2016/NOTE-poe-ucr-20160721/",
+    edDraftURI: "https://w3c.github.io/poe/testsuite/",
+   editors: [{   name:       "Phil Archer",
+         	company:    "W3C/ERCIM"}
+         	],
+    wg: "Permissions &amp; Obligations Expression Working Group",
+    wgURI: "https://www.w3.org/2016/poe/",
+    wgPublicList: "public-poe-comments",
+    wgPatentURI: "https://www.w3.org/2004/01/pp-impl/87755/status",
+    inlineCSS: "true",
+	lint: "false",
+    noRecTrack: "true",
+    localBiblio: {
+      "SCHEMA-ORG":{
+        "href":"http://schema.org/",
+        "title":"Schema.org"
+       },
+    },
+    issueBase: "https://www.w3.org/2016/poe/track/issues/"
+};
