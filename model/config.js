a18744d510defabb246324dafca2673319de2f4b
Renato Iannella <r@iannel.la>
2017-05-24 13:14:23 +1000

    Remove Scopes. Added constraint to Party and Asset (in Constraint section). #183

--- model/config.js
+++ model/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
         specStatus: "ED",
         shortName:  "odrl-model",
-        publishDate:          "2017-05-22",
+        publishDate:          "2017-05-24",
         editors: [
             {   name:       "Renato Iannella",
                     url:        "https://au.linkedin.com/in/riannella",
