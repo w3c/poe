a5990e3cdb1ab518b08ce281c700345411bd5b2d
Renato Iannella <r@iannel.la>
2017-05-26 10:09:44 +1000

    Added Namespaces list. XML encoding now informative. Updated intro. #182

--- vocab/config.js
+++ vocab/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
     specStatus: "ED",
     shortName: "odrl-vocab",
-    publishDate:  "2017-05-24",
+    publishDate:  "2017-05-26",
     edDraftURI: "https://w3c.github.io/poe/vocab/",
     editors: [
      {    name:       "Renato Iannella",
