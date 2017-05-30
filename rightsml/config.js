e687c023e3249d97cd36360f5ecf97ca19df7faa
Renato Iannella <r@iannel.la>
2017-05-30 10:19:27 +1000

    setup rightsml profile note

---
+++ rightsml/config.js
@@ -0,0 +1,26 @@
+var respecConfig = {
+    specStatus: "ED",
+    shortName: "odrl-rightsml",
+    publishDate: "2017-05-30",
+    edDraftURI: "https://w3c.github.io/poe/rightsml/",
+    editors: [ {    name:       "Michael Steidl",
+                    url:        "https://uk.linkedin.com/in/michaelwsteidl",
+                    company:    "Invited Expert",
+                    mailto:     "mdirector@iptc.org"
+                },
+              {  name:       "Stuart Myles",
+                    url:        "https://www.linkedin.com/in/stuart-myles-60a985",
+                    company:    "Invited Expert",
+                    mailto:     "SMyles@ap.org"
+                }
+             ],
+    wg: "Permissions &amp; Obligations Expression Working Group",
+    wgURI: "https://www.w3.org/2016/poe/",
+    wgPublicList: "public-poe-comments",
+    wgPatentURI: "http://www.w3.org/2004/01/pp-impl/87755/status",
+    inlineCSS: true,
+    noIDLIn: true,
+    noLegacyStyle: false,
+    noRecTrack: true,
+    issueBase: "https://www.w3.org/2016/poe/track/issues/"
+};
