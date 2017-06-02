ba08816cd3f0eeb38e083e802d070c75c40316a6
Renato Iannella <r@iannel.la>
2017-06-02 15:35:25 +1000

    Added narrowerThan, implies, partOf properties #160

--- model/config.js
+++ model/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
         specStatus: "ED",
         shortName:  "odrl-model",
-        publishDate:          "2017-05-30",
+        publishDate:          "2017-06-02",
         editors: [
             {   name:       "Renato Iannella",
                     url:        "https://au.linkedin.com/in/riannella",
--- vocab/config.js
+++ vocab/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
     specStatus: "ED",
     shortName: "odrl-vocab",
-    publishDate:  "2017-05-30",
+    publishDate:  "2017-06-02",
     edDraftURI: "https://w3c.github.io/poe/vocab/",
     editors: [
         {    name:       "Renato Iannella",
