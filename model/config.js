e570ffb1ef8b71d6cb887f0ff56283355b90b65c
Renato Iannella <r@iannel.la>
2017-06-06 12:42:20 +1000

    Updated relation/function for conformance #181

--- model/config.js
+++ model/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
         specStatus: "ED",
         shortName:  "odrl-model",
-        publishDate:          "2017-06-02",
+        publishDate:          "2017-06-06",
         editors: [
             {   name:       "Renato Iannella",
                     url:        "https://au.linkedin.com/in/riannella",
