385734bce8ffa01da22c99b050f57bb4a6499da2
Renato Iannella <r@iannel.la>
2017-06-09 10:44:32 +1000

    Updated hasPolicy narrative and skos in vocab #188

--- model/config.js
+++ model/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
         specStatus: "ED",
         shortName:  "odrl-model",
-        publishDate:          "2017-06-08",
+        publishDate:          "2017-06-09",
         editors: [
             {   name:       "Renato Iannella",
                     url:        "https://au.linkedin.com/in/riannella",
