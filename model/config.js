e88abc364ffb53b676770b23ac1f829ffb5d00b6
Renato Iannella <r@iannel.la>
2017-06-14 12:07:32 +1000

    Updated list of Duty requirements #162

--- model/config.js
+++ model/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
         specStatus: "ED",
         shortName:  "odrl-model",
-        publishDate:          "2017-06-13",
+        publishDate:          "2017-06-14",
         editors: [
             {   name:       "Renato Iannella",
                     url:        "https://au.linkedin.com/in/riannella",
