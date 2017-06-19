dc4065d2d37d1c521100f158489dfa2683a1730a
Renato Iannella <r@iannel.la>
2017-06-19 14:31:07 +1000

    Updated constraint narrative and xml #193

--- vocab/config.js
+++ vocab/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
     specStatus: "ED",
     shortName: "odrl-vocab",
-    publishDate:  "2017-06-15",
+    publishDate:  "2017-06-19",
     edDraftURI: "https://w3c.github.io/poe/vocab/",
     editors: [
         {    name:       "Renato Iannella",
