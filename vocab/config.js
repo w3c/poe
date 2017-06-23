35f9f807eaa82485ed06dd6b62534180d8af36e1
Renato Iannella <r@iannel.la>
2017-06-23 12:27:46 +1000

    Update CC terms #158

--- vocab/config.js
+++ vocab/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
     specStatus: "ED",
     shortName: "odrl-vocab",
-    publishDate:  "2017-06-19",
+    publishDate:  "2017-06-23",
     edDraftURI: "https://w3c.github.io/poe/vocab/",
     editors: [
         {    name:       "Renato Iannella",
