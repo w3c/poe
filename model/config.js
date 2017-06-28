f3fcf6ee357beee314d79bcead84a6894a6e46a6
Renato Iannella <r@iannel.la>
2017-06-28 11:36:07 +1000

    Added compact policy #202

--- model/config.js
+++ model/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
         specStatus: "ED",
         shortName:  "odrl-model",
-        publishDate:          "2017-06-27",
+        publishDate:          "2017-06-28",
         editors: [
             {   name:       "Renato Iannella",
                     url:        "https://au.linkedin.com/in/riannella",
