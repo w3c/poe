c4c248126cc92ba699c71aaf7486da2f987af883
Renato Iannella <r@iannel.la>
2017-07-04 12:20:31 +1000

    Removed inheritAllowed property and support multiple inheritance #204

--- model/config.js
+++ model/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
         specStatus: "ED",
         shortName:  "odrl-model",
-        publishDate:          "2017-06-30",
+        publishDate:          "2017-07-04",
         editors: [
             {   name:       "Renato Iannella",
                     url:        "https://au.linkedin.com/in/riannella",
