33df1fe93fb170514606de61787f45d3e636e353
Renato Iannella <r@iannel.la>
2017-07-19 11:25:07 +1000

    updated play/display/print/present terms #192

--- vocab/config.js
+++ vocab/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
     specStatus: "ED",
     shortName: "odrl-vocab",
-    publishDate:  "2017-07-18",
+    publishDate:  "2017-07-19",
     edDraftURI: "https://w3c.github.io/poe/vocab/",
     editors: [
         {    name:       "Renato Iannella",
