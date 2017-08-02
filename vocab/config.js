12830fd681c96ce6b95d61c2466a1594a969d6db
Renato Iannella <r@iannel.la>
2017-08-02 14:03:54 +1000

    Updated Logical constraints to support ordered collection #206

--- vocab/config.js
+++ vocab/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
     specStatus: "ED",
     shortName: "odrl-vocab",
-    publishDate:  "2017-08-01",
+    publishDate:  "2017-08-02",
     edDraftURI: "https://w3c.github.io/poe/vocab/",
     editors: [
         {    name:       "Renato Iannella",
