c1c39e9ff8888010e7d0835cdb2725047b4d3b6b
Renato Iannella <r@iannel.la>
2017-08-09 11:04:05 +1000

    Updates for Composition/Compact #215

--- model/config.js
+++ model/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
         specStatus: "ED",
         shortName:  "odrl-model",
-        publishDate:          "2017-08-08",
+        publishDate:          "2017-08-09",
         editors: [
             {   name:       "Renato Iannella",
                     url:        "https://au.linkedin.com/in/riannella",
