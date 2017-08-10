ada162fda3f4d5a181fdf903cbd0d495e9104aba
Renato Iannella <r@iannel.la>
2017-08-10 12:25:07 +1000

    Changes as per #216

--- vocab/config.js
+++ vocab/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
     specStatus: "ED",
     shortName: "odrl-vocab",
-    publishDate:  "2017-08-09",
+    publishDate:  "2017-08-10",
     edDraftURI: "https://w3c.github.io/poe/vocab/",
     editors: [
         {    name:       "Renato Iannella",
