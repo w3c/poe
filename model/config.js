2405decca33d17eb90f45099264149c0069bca99
Renato Iannella <r@iannel.la>
2017-08-10 12:00:29 +1000

    Cardinality updates as per #215

--- model/config.js
+++ model/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
         specStatus: "ED",
         shortName:  "odrl-model",
-        publishDate:          "2017-08-09",
+        publishDate:          "2017-08-10",
         editors: [
             {   name:       "Renato Iannella",
                     url:        "https://au.linkedin.com/in/riannella",
