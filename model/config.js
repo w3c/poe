b1fe48d08d08e8de32e4461491089ffbf7c117c5
Renato Iannella <r@iannel.la>
2017-08-14 11:29:16 +1000

    Fixed implied section as per #215

--- model/config.js
+++ model/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
         specStatus: "ED",
         shortName:  "odrl-model",
-        publishDate:          "2017-08-11",
+        publishDate:          "2017-08-14",
         editors: [
             {   name:       "Renato Iannella",
                     url:        "https://au.linkedin.com/in/riannella",
