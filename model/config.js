46cd4aeda247ac0b94b992b0fcdf16a7a60bda47
Renato Iannella <r@iannel.la>
2017-08-15 10:38:47 +1000

    Updated abstract #219

--- model/config.js
+++ model/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
         specStatus: "ED",
         shortName:  "odrl-model",
-        publishDate:          "2017-08-14",
+        publishDate:          "2017-08-15",
         editors: [
             {   name:       "Renato Iannella",
                     url:        "https://au.linkedin.com/in/riannella",
