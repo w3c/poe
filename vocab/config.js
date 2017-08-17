c70e2aa63a2abec5e8338867f4ffe890cfb1375c
Renato Iannella <r@iannel.la>
2017-08-17 11:42:22 +1000

    Added Failure property #209

--- vocab/config.js
+++ vocab/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
     specStatus: "ED",
     shortName: "odrl-vocab",
-    publishDate:  "2017-08-12",
+    publishDate:  "2017-08-17",
     edDraftURI: "https://w3c.github.io/poe/vocab/",
     editors: [
         {    name:       "Renato Iannella",
