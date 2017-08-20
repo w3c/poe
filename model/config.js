58c862f6fbf97a7e773c480124d813ace657234f
Renato Iannella <r@iannel.la>
2017-08-20 22:10:31 +1000

    fixed typos #220

--- model/config.js
+++ model/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
         specStatus: "ED",
         shortName:  "odrl-model",
-        publishDate:          "2017-08-17",
+        publishDate:          "2017-08-20",
         editors: [
             {   name:       "Renato Iannella",
                     url:        "https://au.linkedin.com/in/riannella",
