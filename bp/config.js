c70a2ed0a847a21a83762780fbebdf4ea2c9b1c6
Ivan Herman <ivan@ivan-herman.net>
2018-02-05 07:00:49 +0100

    Some minor changes on the config.js, and handling a validation error in the index file

--- bp/config.js
+++ bp/config.js
@@ -1,9 +1,9 @@
 var respecConfig = {
     specStatus: "ED",
     shortName: "odrl-bp",
-     publishDate:          "2016-10-18",
+    // publishDate:          "2016-10-18",
     edDraftURI: "https://w3c.github.io/poe/bp/",
-   editors: [	{   name:       "Benedict Whittam Smith",
+    editors: [	{   name:       "Benedict Whittam Smith",
          	company:    "Thomson Reuters"},
          	{   name:       "Paul Jessop",
          	company:    "Recording Industry Association of America"},
@@ -13,15 +13,30 @@ var respecConfig = {
                    companyURL: "http://www.upm.es",
                     mailto:     "vrodriguez@fi.upm.es"
            }
-         	],
+    ],
     wg: "Permissions &amp; Obligations Expression Working Group",
     wgURI: "https://www.w3.org/2016/poe/",
+    edDraftURI:   "https://w3c.github.io/poe/bp/",
     wgPublicList: "public-poe-comments",
     wgPatentURI: "http://www.w3.org/2004/01/pp-impl/87755/status",
     inlineCSS: true,
     noIDLIn: true,
     noLegacyStyle: false,
     noRecTrack: true,
-    lint: false,
-    issueBase: "https://www.w3.org/2016/poe/track/issues/"
+    lint: true,
+    issueBase: "https://www.w3.org/2016/poe/track/issues/",
+    otherLinks: [
+        {
+            key: "Repository",
+            data: [ {
+                value: "Github Repository",
+                href: "https://github.com/w3c/poe"
+            },
+                {
+                    value: "Issue Tracker",
+                    href: "https://github.com/w3c/poe/issues?q=is%3Aopen+is%3Aissue+label%3A%22Best+Practices%22"
+                } ]
+        }
+    ]
+
 };
